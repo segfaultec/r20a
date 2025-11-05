2e2dbd0700eb0d370c911bd24c30a158cf11f692
ethan <8bitbanana.ec@gmail.com>
2025-11-05 22:34:03 +0000

    New class to encapsulate status edits, ready for stage/unstage

--- markermenu.js
+++ markermenu.js
@@ -54,12 +54,155 @@ function on_selected_token_modified_jumper(token, new_value, event) {
     window.r20a.on_selected_token_modified(token);
 }
 
+var R20A_StatusEditor = class {
+    set_status = null;
+    get_status = null;
+    debug_label = "?"
+
+    constructor(setter, getter)
+    {
+        this.set_status = setter;
+        this.get_status = getter;
+    }
+
+    static from_token(token, model_key) {
+        let setter = (new_status) => {
+            let payload = {}
+            payload[model_key] = new_status
+            token.model.save(payload)
+        };
+        let getter = () => {
+            return token.model.get(model_key)
+        };
+        let obj = new this(setter, getter);
+        obj.debug_label = token.model.get("name") + "_" + model_key;
+        return obj;
+    }
+
+    add_status(statusid, message) {
+        if (message) {
+            message = message.replaceAll("@","").replaceAll(",","");
+        }
+
+        const old_statuses = this.get_status();
+
+        const old_status_list = old_statuses
+            .split(",")
+            .map((s) => s.split("@")[0]);
+
+        if (typeof old_status_list.find(statusid) === "undefined") {
+            const new_status_entry = (message ? `${statusid}@${message}` : statusid);
+
+            let new_statuses;
+            if (old_statuses) {
+                new_statuses = old_statuses + "," + new_status_entry;
+            } else {
+                new_statuses = new_status_entry;
+            }
+
+            this.update_token_status(new_statuses, "add");
+        }
+    }
+
+    remove_status(statusid) {
+        let removed_elements = []
+
+        let new_statuses = this.get_status()
+            .split(",")
+            .filter((s) => {
+                if (s.split("@")[0] === statusid) {
+                    removed_elements.push(s);
+                    return false;
+                } else {
+                    return true;
+                }
+            })
+            .join(",");
+        this.update_token_status(new_statuses, "remove");
+
+        return removed_elements;
+    }
+
+    bump_numeric_status(statusid, amount) {
+        let new_statuses = this.get_status()
+            .split(",")
+            .map((s) => {
+                let split = s.split("@");
+                if (split[0] === statusid && split[1]) {
+                    const match = split[1].match("(-?\\d+)(ft)?");
+                    if (match) {
+                        let number = parseInt(match[1])
+                        let bump_amount = amount;
+                        // DnD height: hardcode to bump in increments of 5
+                        if (match[2] === "ft") {
+                            bump_amount *= 5;
+                        }
+                        return split[0] + "@" + split[1].replace(number, number + bump_amount);
+                    }
+                }
+                return s;
+            })
+            .join(",")
+        this.update_token_status(new_statuses, "bump");
+    }
+
+    edit_status(statusid, new_message) {
+        if (new_message) {
+            new_message = new_message.replaceAll("@","").replaceAll(",","");
+        }
+        let new_statuses = this.get_status()
+            .split(",")
+            .map((s) => {
+                let split = s.split("@");
+                if (split[0] === statusid) {
+                    if (new_message) {
+                        return split[0] + "@" + new_message;
+                    } else {
+                        return split[0];
+                    }
+                } else {
+                    return s;
+                }
+            })
+            .join(",");
+        this.update_token_status( new_statuses, "edit");
+    }
+
+    reorder_status(statusid, new_index) {
+        let popped_status = null;
+        let new_status = Array.from(this.get_status()
+            .split(",")
+            .filter((value, index) => {
+                if (value.split("@")[0] === statusid) {
+                    popped_status = value;
+                    return false;
+                }
+                return true;
+            }));
+
+        if (popped_status) {
+            new_status.insert(popped_status, new_index);
+            new_status = new_status.join(",");
+
+            this.update_token_status(new_status, "reorder");
+        }
+    }
+
+    update_token_status(new_status, op) {
+        if (r20a.log) {
+            const old_status = this.get_status();
+            console.info(`r20a update: op:${op} token:"${this.debug_label}" old:"${old_status}" new:"${new_status}"`);
+        }
+        this.set_status(new_status);
+    }
+}
+
 var R20A = class {
     engine = null;
     overlay = null;
     current_selected_tokens = [];
     statusicons = {};
-    log = false;
+    log = true;
 
     // flag so we don't rerender the markermenu when typing in the edit textbox
     // gets incremented once for each token, so it decrements back to 0 when each token gets modified
@@ -348,115 +491,52 @@ var R20A = class {
             message = message.replaceAll("@","").replaceAll(",","");
         }
         this.current_selected_tokens.forEach((token) => {
-            const old_statuses = token.model.get("statusmarkers");
-
-            const old_status_list = old_statuses
-                .split(",")
-                .map((s) => s.split("@")[0]);
-
-            if (typeof old_status_list.find(statusid) === "undefined") {
-                const new_status_entry = (message ? `${statusid}@${message}` : statusid);
-
-                let new_statuses;
-                if (old_statuses) {
-                    new_statuses = old_statuses + "," + new_status_entry;
-                } else {
-                    new_statuses = new_status_entry;
-                }
-
-                this.update_token_status(token, new_statuses, "add");
-            }
+            R20A_StatusEditor.from_token(token, "statusmarkers").add_status(statusid, message);
         });
     }
 
     remove_status(statusid) {
         this.current_selected_tokens.forEach((token) => {
-            let new_statuses = token.model.get("statusmarkers")
-                .split(",")
-                .filter((s) => s.split("@")[0] !== statusid)
-                .join(",");
-            this.update_token_status(token, new_statuses, "remove");
+            R20A_StatusEditor.from_token(token, "statusmarkers").remove_status(statusid);
         });
     }
 
     bump_numeric_status(statusid, amount) {
         this.current_selected_tokens.forEach((token) => {
-            let new_statuses = token.model.get("statusmarkers")
-                .split(",")
-                .map((s) => {
-                    let split = s.split("@");
-                    if (split[0] === statusid && split[1]) {
-                        const match = split[1].match("(-?\\d+)(ft)?");
-                        if (match) {
-                            let number = parseInt(match[1])
-                            let bump_amount = amount;
-                            // DnD height: hardcode to bump in increments of 5
-                            if (match[2] === "ft") {
-                                bump_amount *= 5;
-                            }
-                            return split[0] + "@" + split[1].replace(number, number + bump_amount);
-                        }
-                    }
-                    return s;
-                })
-                .join(",")
-            this.update_token_status(token, new_statuses, "bump");
+            R20A_StatusEditor.from_token(token, "statusmarkers").bump_numeric_status(statusid, amount);
         });
     }
 
     edit_status(statusid, new_message) {
-        if (new_message) {
-            new_message = new_message.replaceAll("@","").replaceAll(",","");
-        }
         this.current_selected_tokens.forEach((token) => {
-            let new_statuses = token.model.get("statusmarkers")
-                .split(",")
-                .map((s) => {
-                    let split = s.split("@");
-                    if (split[0] === statusid) {
-                        if (new_message) {
-                            return split[0] + "@" + new_message;
-                        } else {
-                            return split[0];
-                        }
-                    } else {
-                        return s;
-                    }
-                })
-                .join(",");
-            this.update_token_status(token, new_statuses, "edit");
+            R20A_StatusEditor.from_token(token, "statusmarkers").edit_status(statusid, new_message);
         });
     }
 
     reorder_status(statusid, new_index) {
         this.current_selected_tokens.forEach((token) => {
-            let popped_status = null;
-            let new_status = Array.from(token.model.get("statusmarkers")
-                .split(",")
-                .filter((value, index) => {
-                    if (value.split("@")[0] === statusid) {
-                        popped_status = value;
-                        return false;
-                    }
-                    return true;
-                }));
-
-            if (popped_status) {
-                new_status.insert(popped_status, new_index);
-                new_status = new_status.join(",");
-
-                this.update_token_status(token, new_status, "reorder");
-            }
+            R20A_StatusEditor.from_token(token, "statusmarkers").reorder_status(statusid, new_index);
         });
     }
 
-    update_token_status(token, new_status, op) {
-        if (this.log) {
-            const old_status = token.model.get("statusmarkers");
-            const name = token.model.get("name");
-            console.info(`r20a update: op:${op} token:"${name}" old:"${old_status}" new:"${new_status}"`);
-        }
-        token.model.save({statusmarkers:new_status});
+    stash_status(statusid) {
+        this.current_selected_tokens.forEach((token) => {
+            let removed = R20A_StatusEditor.from_token(token, "statusmarkers").remove_status(statusid, new_index);
+            removed.forEach((s) => {
+                let s_split = s.split("@");
+                R20A_StatusEditor.from_token(token, "statusmarkers_r20a_stash").add_status(s_split[0], s_split[1]);
+            })
+        });
+    }
+
+    unstash_status(statusid) {
+        this.current_selected_tokens.forEach((token) => {
+            let removed = R20A_StatusEditor.from_token(token, "statusmarkers_r20a_stash").remove_status(statusid, new_index);
+            removed.forEach((s) => {
+                let s_split = s.split("@");
+                R20A_StatusEditor.from_token(token, "statusmarkers").add_status(s_split[0], s_split[1]);
+            })
+        });
     }
 }
 
